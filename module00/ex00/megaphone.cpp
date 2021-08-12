771802bd3f1a48ae6807da2706a444f440c9766d
mzomeno- <misaclumsy@gmail.com>
2021-08-12 14:42:37 +0200

    Makefile working

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/12 14:24:00 by mzomeno-          #+#    #+#             */
+/*   Updated: 2021/08/12 14:28:00 by mzomeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+#define DEFAULT_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+
+int main()
+{
+	cout << DEFAULT_MSG << endl;
+	return (0);
+}
