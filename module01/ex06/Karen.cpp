f79b261c0100c7fb006f7d6d35188d9591762d6f
mzomeno- <mzomeno-@student.42madrid.com>
2021-08-22 19:16:40 +0200

    Added Makefile and copied Karen class files

---
+++ module01/ex06/Karen.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/22 15:29:28 by mzomeno-          #+#    #+#             */
+/*   Updated: 2021/08/22 16:05:17 by mzomeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+Karen::Karen()
+{
+}
+
+Karen::~Karen()
+{
+}
+
+void	Karen::debug()
+{
+	std::cout << \
+		"I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" \
+		<< std::endl;
+}
+
+void	Karen::info()
+{
+	std::cout << \
+		"I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" \
+		<< std::endl;
+}
+
+void	Karen::warning()
+{
+	std::cout << \
+		"I think I deserve to have some extra bacon for free."\
+	   	<< " I’ve been coming here for years and you just started working here last month." \
+		<< std::endl;
+}
+
+void	Karen::error()
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void	Karen::complain(std::string level)
+{
+	KarenFisher functions[4] = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+	std::string labels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (labels[i] == level)
+			(this->*functions[i])();
+	}
+}
---
+++ module01/ex06/Karen.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/22 15:29:14 by mzomeno-          #+#    #+#             */
+/*   Updated: 2021/08/22 16:03:14 by mzomeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KAREN_H
+# define KAREN_H
+
+#include <iostream>
+
+class Karen
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+
+	public:
+		Karen();
+		~Karen();
+
+		void complain( std::string level );
+};
+
+typedef	void	(Karen::*KarenFisher)(void);
+
+#endif
