48e6eebb8f8f6250a844c07ec84cb996f874a134
mzomeno- <misaclumsy@gmail.com>
2021-08-22 16:09:07 +0200

    Completed main

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 15:44:14 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/08/22 15:46:06 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/08/22 16:08:35 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,17 @@ int main()
 {
 	Karen exampleKaren;
 
+	std::cout << "[DEBUG] Karen: ";
 	exampleKaren.complain("DEBUG");
+	
+	std::cout << "[INFO] Karen: ";
+	exampleKaren.complain("INFO");
+	
+	std::cout << "[WARNING] Karen: ";
+	exampleKaren.complain("WARNING");
+	
+	std::cout << "[ERROR]: Karen: ";
+	exampleKaren.complain("ERROR");
 	
 	return 0;
 }
