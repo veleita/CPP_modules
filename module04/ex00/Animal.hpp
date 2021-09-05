8fee72aa3464b053cc9564f2a5a467127db43e31
mzomeno- <misaclumsy@gmail.com>
2021-09-05 16:19:16 +0200

    Animal classes tested

--- module04/ex00/Animal.hpp
+++ module04/ex00/Animal.hpp
@@ -6,12 +6,15 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 14:39:21 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/08/30 14:59:01 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/09/05 16:14:55 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ANIMAL_H
 # define ANIMAL_H
+
+# include <iostream>
+
 
 class Animal
 {
@@ -24,33 +27,37 @@ class Animal
 
 		Animal	&operator=(Animal const &rhs);
 
-		std::string	&getType(void) const;
+		std::string	getType(void) const;
 
-		~Animal();
+		virtual void	makeSound() const;
+
+		virtual ~Animal();
 };
 
 class Dog : public Animal
 {
 	public:
 		Dog();
-		Dog(std::string &type);
 		Dog(Dog const &copy);
 
 		Dog	&operator=(Dog const &rhs);
 
 		void	makeSound() const;
+
+		~Dog();
 };
 
 class Cat : public Animal
 {
 	public:
 		Cat();
-		Cat(std::string &type);
 		Cat(Cat const &copy);
 
 		Cat	&operator=(Cat const &rhs);
 
 		void	makeSound() const;
+
+		~Cat();
 };
 
 #endif
--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -6,12 +6,35 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:35:32 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/08/30 13:36:04 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/09/05 16:17:31 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Animal.hpp"
+
 int main()
 {
+	std::cout << "CONSTRUCTORS:" << std::endl;
+	const Animal* meta = new Animal();
+	const Animal* puppy = new Dog();
+	const Animal* kitten = new Cat();
+	std::cout << std::endl;
+	
+	std::cout << "TYPES:" << std::endl;
+	std::cout << puppy->getType() << " " << std::endl;
+	std::cout << kitten->getType() << " " << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "MAKE SOUND:" << std::endl;
+	puppy->makeSound();
+	kitten->makeSound();
+	meta->makeSound();
+	std::cout << std::endl;
+
+	std::cout << "DESTRUCTORS:" << std::endl;
+	delete meta;
+	delete puppy;
+	delete kitten;
 
 	return (0);
 }
