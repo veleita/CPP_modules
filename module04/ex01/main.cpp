0e9e774d59d7f45738dcb4502ddca38191b7dbaf
mzomeno- <misaclumsy@gmail.com>
2021-09-05 18:32:27 +0200

    Need to write getters and setters in Cat and Dog classes

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 16:52:49 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/09/05 18:25:07 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/09/05 18:31:39 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,17 @@ int main()
 	array[0] = doggo;
 	array[1] = puss;
 
+	doggo.setIdeas(0, "I wanna play! Can we play?");
+	puss.setIdeas(0, "I'm hungry");
+
+	Dog	dogCopy(doggo);
+	Cat	catCopy(puss);
+
+	std::cout << doggo->getIdeas(0) << std::endl; 
+	std::cout << puss->getIdeas(0) << std::endl; 
+	std::cout << dogCopy->getIdeas(0) << std::endl; 
+	std::cout << catCopy->getIdeas(0) << std::endl; 
+	
 	for (int i = 0; i < 2; i++)
 		delete array[i];
 
