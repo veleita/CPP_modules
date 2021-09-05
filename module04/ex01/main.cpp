6b7ebd8d9e1d8b1ece4ce17a75cad3073556eef6
mzomeno- <misaclumsy@gmail.com>
2021-09-05 18:25:39 +0200

    Created and deleted animal array

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 16:52:49 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/09/05 17:32:15 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/09/05 18:25:07 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ int main()
 	const Animal* doggo = new Dog();
 	const Animal* puss = new Cat();
 
-	delete doggo;
-	delete puss;
+	const Animal *array[2];
+
+	array[0] = doggo;
+	array[1] = puss;
+
+	for (int i = 0; i < 2; i++)
+		delete array[i];
+
 	return 0;
 }
