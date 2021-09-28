a4fe1ed4a6c9f94dae97099077010313c484f84f
mzomeno- <misaclumsy@gmail.com>
2021-09-28 10:32:41 +0200

    Complete

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 19:44:52 by mzomeno-          #+#    #+#             */
-/*   Updated: 2021/09/27 19:22:56 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2021/09/28 10:32:06 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,30 @@ int main( void )
 	stringArray[2] = "foo foo";
 	for (int i = 0; i < 3; i++)
 		std::cout << stringArray[i] << std::endl;
+	std::cout << std::endl;
+
+	intArray[0] = 5;
+	intArray[1] = 6;
+	intArray[2] = 7;
+	for (int j = 0; j < 3; j++)
+		std::cout << intArray[j] << std::endl;
+	std::cout << std::endl;
+
+	floatArray[0] = 0.098f;
+	floatArray[1] = 9.9985f;
+	floatArray[2] = 654.7f;
+	for (int k = 0; k < 3; k++)
+		std::cout << floatArray[k] << std::endl;
+	std::cout << std::endl;
 
 	std::cout << std::endl;
 
 	std::cout << "MEMBER FUNCTION" << std::endl;
 	std::cout << "-------------------" << std::endl;
 
-	std::cout << std::endl;
-
-	std::cout << "DESTRUCTORS" << std::endl;
-	std::cout << "-------------------" << std::endl;
-
-	std::cout << std::endl;
+	std::cout << stringArray.size() << std::endl;
+	std::cout << intArray.size() << std::endl;
+	std::cout << floatArray.size() << std::endl;
 
 	return 0;
 }
