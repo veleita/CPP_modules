f1283d50dea685f6023ebc82a7937014e8fadd52
María Zomeño <maria.zomenogutierrez@telefonica.com>
2023-03-27 19:55:14 +0200

    Makefile and input error

---
+++ module09/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzomeno- <mzomeno-@42madrid.student.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 19:53:22 by mzomeno-          #+#    #+#             */
+/*   Updated: 2023/03/27 19:54:00 by mzomeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+#endif
---
+++ module09/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzomeno- <mzomeno-@42madrid.student.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 19:44:35 by mzomeno-          #+#    #+#             */
+/*   Updated: 2023/03/27 19:53:06 by mzomeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Error: could not open file." << std::endl;
+        exit(1);
+    }
+	return (0);
+}
