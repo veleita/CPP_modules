9ee8cd3365a2c04fbcd539abb10328f801e948a5
María Zomeño <maria.zomenogutierrez@telefonica.com>
2023-04-17 13:16:01 +0200

    Set input read methods

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <mzomeno-@42madrid.student.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 19:36:26 by mzomeno-          #+#    #+#             */
-/*   Updated: 2023/04/17 12:03:52 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2023/04/17 13:14:59 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ float getRate(std::string database)
     return rate;
 }
 
-BitcoinExchange::BitcoinExchange (std::string inputFile)
+void    BitcoinExchange::_loadDatabase()
 {
     std::ifstream   input;
     std::string     dataRow;
     std::string     date;
     float           rate;
 
-    input.open(inputFile);
+    input.open("./data.csv");
     while (!input.eof())
     {
         input >> dataRow;
@@ -45,13 +45,37 @@ BitcoinExchange::BitcoinExchange (std::string inputFile)
     input.close();
 }
 
+void    BitcoinExchange::_readInput(std::string inputFile)
+{
+    std::ifstream   input;
+    input.open(inputFile);
+}
+
+void    BitcoinExchange::_parseInput()
+{
+}
+
+void    BitcoinExchange::printOutput()
+{
+}
+
+std::map<std::string , float>   BitcoinExchange::getDatabase()
+{
+    return this->_database;
+}
+
+BitcoinExchange::BitcoinExchange (std::string inputFile)
+{
+    this->_loadDatabase();
+    this->_readInput(inputFile);
+    this->_parseInput();
+    this->printOutput();
+}
+
 BitcoinExchange::BitcoinExchange (const BitcoinExchange &copy)
 {
     *this = copy;
 }
-
-BitcoinExchange::~BitcoinExchange ()
-{}        
 
 BitcoinExchange & BitcoinExchange::operator = (const BitcoinExchange &rhs)
 {
@@ -59,7 +83,5 @@ BitcoinExchange & BitcoinExchange::operator = (const BitcoinExchange &rhs)
     return *this;
 }
 
-std::map<std::string , float>   BitcoinExchange::getDatabase()
-{
-    return this->_database;
-}
+BitcoinExchange::~BitcoinExchange ()
+{}        
--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <mzomeno-@42madrid.student.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:53:22 by mzomeno-          #+#    #+#             */
-/*   Updated: 2023/04/03 20:29:57 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2023/04/17 13:15:18 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,21 @@
 class BitcoinExchange
 {
     private:
-        std::map<std::string , float> _database;
+        std::map<std::string , float>    _database;
+        void                             _loadDatabase();
+        
+        void    _readInput(std::string inputFile);
+        void    _parseInput();
 
     public:
         BitcoinExchange (std::string inputFile);
         BitcoinExchange (const BitcoinExchange &copy);
+        BitcoinExchange & operator = (const BitcoinExchange &rhs);
         ~BitcoinExchange ();
         
-        BitcoinExchange & operator = (const BitcoinExchange &rhs);
-        
         std::map<std::string , float>   getDatabase();
+        
+        void    printOutput();
 };
 
 #endif
